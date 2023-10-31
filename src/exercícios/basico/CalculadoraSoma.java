12896987aebd1ecf7702e8122d4a60b25077b11a
Camila Rossi <116846050+camilahrossii@users.noreply.github.com>
2023-10-31 16:51:38 -0300

    add files

---
+++ src/exercícios/basico/CalculadoraSoma.java
@@ -0,0 +1,22 @@
+package exercícios.basico;
+
+import java.util.Scanner;
+
+public class CalculadoraSoma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        double soma = numero1 + numero2;
+
+        System.out.println("A soma dos números é: " + soma);
+
+        scanner.close();
+        
+    }
+}
---
+++ src/exercícios/basico/Media.java
@@ -0,0 +1,28 @@
+package exercícios.basico;
+
+import java.util.Scanner;
+
+public class Media {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Vamos calcular a média das notas...");
+
+        System.out.println("Insira a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.println("Insira a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+        
+        System.out.println("Insira a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A média das notas informadas é: " + media);
+
+        scanner.close();
+
+    }
+}
