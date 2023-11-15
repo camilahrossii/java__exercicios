ac9a3d1c8cc88cb4cbe381210e51e82db79a5ae3
Camila Rossi <camilarossi.contact@gmail.com>
2023-11-15 08:57:28 -0300

    fix: move folder 'intermediario' to src

---
+++ src/exercícios/intermediario/Fatorial.java
@@ -0,0 +1,30 @@
+package exercícios.intermediario;
+
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe um número para calcular o Fatorial: ");
+        int numero = scanner.nextInt();
+
+        long fatorial = calcularFatorial(numero);
+        System.out.println("O fatorial do número " + numero + " é " + fatorial);
+    }
+
+    public static long calcularFatorial(int numero) {
+        if(numero < 0) {
+            return -1;
+        } 
+
+        long fatorial = 1;
+
+        for(int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        return fatorial;
+    }
+
+}
